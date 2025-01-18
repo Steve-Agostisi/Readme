d2316dd8c3fb688c4dee4a84b44e74465769c088
Steven Agostisi <sagostisi@gmail.com>
2025-01-18 14:05:16 -0500

    Readme

---
+++ index.js
@@ -0,0 +1,78 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What is the usage information for this project?',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What is the license for this project?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What are the contributing guidelines?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What are the test instructions?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+]; 
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log('Error writing to file:', err);
+    } else {
+      console.log('README file has been generated!');
+    }
+  });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions)
+  .then((answers) => {
+    const readmeContent = generateREADME(answers);
+    writeToFile('README.md', readmeContent);
+  })
+  .catch((error) => {
+    console.log('Error:', error);
+  });
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,80 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+
+  const licenseBadges = {
+    MIT: '![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)',
+    Apache: '![Apache License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)',
+    GPL: '![GPL License](https://img.shields.io/badge/license-GPL-blue.svg)',
+    BSD: '![BSD License](https://img.shields.io/badge/license-BSD-blue.svg)',
+    // Add more licenses as needed
+  };
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+
+  const licenseLinks = {
+    MIT: 'https://opensource.org/licenses/MIT',
+    Apache: 'https://opensource.org/licenses/Apache-2.0',
+    GPL: 'https://www.gnu.org/licenses/gpl-3.0',
+    BSD: 'https://opensource.org/licenses/BSD-3-Clause',
+    // Add more licenses as needed
+  };
+
+  return licenseLinks[license] || ''; 
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `
+## License
+This project is licensed under the ${license} License. For more details, see the [license link](${renderLicenseLink(license)}).
+  `;
+
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact me at [${data.email}](mailto:${data.email}).
+GitHub: [${data.github}](https://github.com/${data.github})
+
+`;
+}
+
+export default generateMarkdown;
